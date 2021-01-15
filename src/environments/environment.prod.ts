544309b1c447a6aa09b26c59d7a96281352ae02d
lamhung <lamhungypl@gmail.com>
2021-01-15 10:07:12 +0700

    api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  storeUrl: '',
-  imageUrl: '',
+  storeUrl: 'http://ec2-18-232-149-110.compute-1.amazonaws.com:8000/api/',
+  imageUrl: 'http://ec2-18-232-149-110.compute-1.amazonaws.com:8000/api/media/image-resize',
 };
